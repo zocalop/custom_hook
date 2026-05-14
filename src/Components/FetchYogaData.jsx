031d03917109e984b07cab2aa7d5a196e230477b
zocalop <jcb2861@gmail.com>
2026-05-14 12:31:07 -0300

    Updated FetchYogaData.jsx in main

--- src/Components/FetchYogaData.jsx
+++ src/Components/FetchYogaData.jsx
@@ -1,11 +1,24 @@
 import React from 'react';
 import UseFetch from './UseFetch.jsx';
+import './FetchData.css' 
 
 const FetchYogaData = () => {
-  
+  const [data]=UseFetch('https://api.npoint.io/4459a9a10e43812e1152');
+  console.log(data);
   return (
     <>
-    
+      <ul className='list_data_name'>
+        <h1 className='usefetch_heading'>Yoga Benefits</h1>
+        {data && data.map((e,index)=>(
+          <>
+            <li key={index} className='list_data'>
+              <h3>{e.name}</h3>
+              <p><strong>Benefits: </strong>{e.benefits}</p>
+              <p><strong>Duration: </strong>{e.time_duration}</p>
+            </li>
+          </>
+        ))}
+      </ul>    
     </>
   )
 }
