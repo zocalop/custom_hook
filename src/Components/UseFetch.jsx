7adb1c4c7a0aa01a60ef249b094c6ba3a0450684
zocalop <jcb2861@gmail.com>
2026-05-14 11:11:09 -0300

    Updated FetchData.jsx and UseFetch.jsx in main

--- src/Components/UseFetch.jsx
+++ src/Components/UseFetch.jsx
@@ -1,8 +1,13 @@
 
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 const UseFetch = (url) => {
-    const [data, setData] = useState();
+  const [data, setData] = useState();
+  useEffect(()=>{
+    fetch(url).then((res)=>res.json())
+    .then((data)=>setData(data))
+  },[])
+  return [data]
 }
 
 export default UseFetch
